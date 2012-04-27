d63df919d737e72f80dd3d6dc4a37cc9f1b29a65
Riccardo Carlesso <riccardo_carlesso@dell.com>
2012-04-27 08:39:06 +0100

    added person ID in configuration. Wow!
    Added README and other niftieties

--- lib/simple-ruby-facebook-example.rb
+++ lib/simple-ruby-facebook-example.rb
@@ -7,25 +7,28 @@ require 'socket'
 #require 'actionpack' #  action_view/helpers/text_helper.rb
 require 'action_view' #  action_view/helpers/text_helper.rb
 
-
-#include MyLibrary
-
 # register your app at facebook to get those infos
 require APP_ROOT + '/lib/my_facebook_app.rb' # configuration for your app
 
 class SimpleRubyFacebookExample < Sinatra::Application
 
-	include Koala
-	include ActionView::Helpers::TextHelper
-	include ActionView::Helpers::TagHelper
-	include ActionView::Helpers::FormHelper
+  include Koala
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::FormHelper
+  
+  #@@ricFacebookFriend = '100000419195371' # Chiara Gennari
 
-	set :root, APP_ROOT
-	enable :sessions
-	
-	def get_username(graph)
-	 graph.inspect rescue :boh
-	end
+  set :root, APP_ROOT
+  enable :sessions
+
+  def get_username(graph)
+   graph.inspect rescue :boh
+  end
+
+  def log_info()
+    "#{ENV['USER']}@#{Socket.gethostname}"
+  end
 
 	get '/' do
 		if session['access_token']
@@ -35,17 +38,59 @@ class SimpleRubyFacebookExample < Sinatra::Application
 			# publish to your wall (if you have the permissions)
 			#@graph.put_wall_post("I'm posting from my new cool app!")
 			# or publish to someone else (if you have the permissions too ;) )
-			# @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")
-			'You are logged in as <tt>'+ escape_once(get_username(@graph)) +'</tt>! <a href="/logout">Logout</a> <BR/> <a href="/post_on_wall">Post on uoll (attento!)</a>:'
+			
+			html_page('You are logged in as <tt>'+ escape_once(get_username(@graph)) +'</tt>!
+			<a href="/">Posting on Chiara wall</a>
+			<a href="/logout">Logout</a> <BR/>
+			<a href="/post_on_wall">Post on uoll (attento!)</a>:')
 		else
-			'<a href="/login">Login</a>'
+			html_page '<a href="/login">Login</a>'
 		end
 	end
+	
+	def fb_link_for(id)
+	  "FBLINK <a href='https://www.facebook.com/profile.php?id=#{id}' >Facebook Page of #{id} </a>"
+  end
+  
+  def header()
+    "<center>[ 
+      <a href='/login' >Login</a>
+      <a href='/friends' >Friends</a>
+      <a href='/post_on_other_persons_wall?msg=ciao&friend_id=#{@@ricFacebookFriend}' >Posting on Chiara wall</a>
+      <a href='/logout' >Logout</a>
+    ]</center> <h1>#{APPNAME}</h1>"
+  end
+  
+  def footer()
+    "<hr/> <tt>Facebook mini app made in sinatra. See <a href='/README'>README</a></tt>"
+  end
+  
+  def html_page(str)
+    header() + str.to_s + footer()
+  end
 
+  # @@ricFacebookFriend
+  get '/post_on_other_persons_wall' do
+    friend_id = params.fetch :friend_id, @@ricFacebookFriend
+    msg = params.fetch 'msg', "default message"
+    @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
+    @graph.put_wall_post("#{log_info}: #{msg}", {}, friend_id)
+    html_page "Message '#{msg}' correctly published on #{fb_link_for friend_id} wall. DEBUG:_ params are: #{params.inspect}"
+  end
+  
+  get '/friends' do
+    html_page "It would be nice here to show your friends!!!"
+  end
+  
+  get '/README' do
+    html_page( "<pre>" + File.read(APP_ROOT + "/README") + "</pre>" )
+  end
+  
   get '/post_on_wall' do
     @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
-    @graph.put_wall_post("I'm posting from my new cool app bella Ric From: #{ENV['USER']}@#{Socket.gethostname} on #{Time.now}")
-    "Curva mac! Wanna post on wall? Really sure? TODO nicknames (applica nicknames in database locale che matchino il tuo user)"
+    @graph.put_wall_post("I'm posting from my new cool app bella Ric From: #{log_info}")
+    html_page "Curva mac! Wanna post on wall? Really sure? 
+    TODO nicknames (applica nicknames in database locale che matchino il tuo user)"
   end
 
 	get '/login' do
